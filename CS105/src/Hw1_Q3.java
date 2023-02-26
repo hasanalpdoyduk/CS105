b0a5e33481470d885a664656bb0bd7bb3ff8a4ca
Hasan Alp Doyduk <67971540+hasanalpdoyduk@users.noreply.github.com>
2023-02-26 17:49:21 +0300

    Create Hw1_Q3.java

---
+++ CS105/src/Hw1_Q3.java
@@ -0,0 +1,13 @@
+public class Lecture {
+
+	public static void main(String[] args) {
+		
+		double radius = 12.45;
+		double pi = 3.141519;
+		double area = radius * radius * pi;
+		
+		System.out.println((double)((int)(area*1000))/1000);
+		
+	}
+
+}
