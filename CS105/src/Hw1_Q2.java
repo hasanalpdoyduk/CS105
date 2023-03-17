a8b1fdbd7960d393c354f2ddcd8d116566bb8c9b
Hasan Alp Doyduk <67971540+hasanalpdoyduk@users.noreply.github.com>
2023-03-17 15:32:03 +0300

    Update Hw1_Q2.java

--- CS105/src/Hw1_Q2.java
+++ CS105/src/Hw1_Q2.java
@@ -6,27 +6,26 @@ public class Hw1_Q2 {
 
 		Scanner input = new Scanner(System.in);
 		
-        System.out.print("Enter a string: ");
-        String words = input.nextLine().toLowerCase();
+        	System.out.print("Enter a string: ");
+        	String words = input.nextLine().toLowerCase();
         
 
-        int vowelNum = 0;
-        int consonantNum = 0;
-        
-        for (int i = 0; i < words.length(); i++) {
-            String str = words.substring(i, i+1);
-            if (str.matches("[a-z]")) {
-                if (str.matches("[aeiou]")) {
-                    vowelNum++;
-                } else {
-                    consonantNum++;
-                }
-                
-            }
-        }
-        
-        System.out.println("The number of vowels is " + vowelNum);
-        System.out.println("The number of consonants is " + consonantNum);
+		int vowelNum = 0;
+		int consonantNum = 0;
+
+		for (int i = 0; i < words.length(); i++) {
+		    String str = words.substring(i, i+1);
+		    if (str.matches("[a-z]")) {
+			if (str.matches("[aeiou]")) {
+			    vowelNum++;
+			} else {
+			    consonantNum++;
+			}
+
+		    }
+		}
+
+		System.out.println("The number of vowels is " + vowelNum);
+		System.out.println("The number of consonants is " + consonantNum);
 	}
-
 }
